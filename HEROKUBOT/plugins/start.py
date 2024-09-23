804870714b2b0d9b71032499551fca04685fe52f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-24 00:04:36 +0530

    Update start.py

--- HEROKUBOT/plugins/start.py
+++ HEROKUBOT/plugins/start.py
@@ -2,6 +2,6 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["start"]))
 async def start(client, message):
-    await message.reply_text("Hello there! I am heroku control bot")
+    await message.reply_text(f"Hello there! I am heroku control bot\n\nHosting Commands: /host\nDelete hosting: /deletehost\n\nCheck hosted apps: /myhost, /heroku.")
         
     
