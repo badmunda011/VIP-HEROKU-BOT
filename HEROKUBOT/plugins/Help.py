e95684343fdd9911d6d56aecd80401102fb8f00c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-24 03:55:41 +0530

    Update Help.py

--- HEROKUBOT/plugins/Help.py
+++ HEROKUBOT/plugins/Help.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
-from pyrogram import Client as VIP
-@VIP.on_message(filters.command(["help"]))
+from pyrogram import Client as app
+@app.on_message(filters.command(["help"]))
 async def start(client, message):
     await message.reply_text(f"Hello there! I am heroku control bot\n\nHosting Commands: /host\nDelete hosting: /deletehost\n\nCheck hosted apps: /myhost, /heroku.")
         
