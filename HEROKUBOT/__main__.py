db89785076a3a0f0475755fd44d6b0cbc39b4d07
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-23 21:27:58 +0530

    Update __main__.py

--- HEROKUBOT/__main__.py
+++ HEROKUBOT/__main__.py
@@ -1,21 +1,17 @@
 import os
 import logging
 import pyrogram
-from decouple import config
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-# vars
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
 
 if __name__ == "__main__" :
     print("Starting Bot...")
-    plugins = dict(root="PyroBot/plugins")
+    plugins = dict(root="HEROKUBOT/plugins")
     app = pyrogram.Client(
-        "BotzHub",
+        "HEROKUBOT",
         bot_token=BOT_TOKEN,
         api_id=APP_ID,
         api_hash=API_HASH,
