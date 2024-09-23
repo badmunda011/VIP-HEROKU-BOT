ae4997db76f3e83197bc108a555b0258da7e9f90
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-24 01:21:50 +0530

    Update __main__.py

--- HEROKUBOT/__main__.py
+++ HEROKUBOT/__main__.py
@@ -1,29 +1,34 @@
 import os
 import logging
-import pyrogram
+from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-import pyromod.listen   # Import pyromod listen
+import pyromod.listen  # Import pyromod listen
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-if __name__ == "__main__":
-    print("Starting Bot...")
-    
-    plugins = dict(root="HEROKUBOT/plugins")
-    
-    # Initialize the bot client
-    app = pyrogram.Client(
-        "HEROKUBOT",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins
-    )
-    
-    
+# Initialize the bot client
+class HEROKUBot(Client):
+    def __init__(self):
+        super().__init__(
+            "HEROKUBOT",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins=dict(root="HEROKUBOT/plugins"),
+        )
 
     # Run the bot
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.mention = self.me.mention
+        print("Started Bot.")
+
+# To run the bot
+if __name__ == "__main__":
+    app = HEROKUBot()
     app.run()
-    print("Started Bot.")
-app = app()
