f46cf1903e4c509f30e70e408f81c343f732d18f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-23 22:50:30 +0530

    Update start.py

--- HEROKUBOT/plugins/start.py
+++ HEROKUBOT/plugins/start.py
@@ -2,6 +2,6 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["start"]))
 async def start(client, message):
-    await client.send_message(message.chat.id, "Hello there! I am heroku control bot")
+    await message.reply_text("Hello there! I am heroku control bot")
         
     
