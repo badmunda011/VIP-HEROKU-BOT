6294ce487047b2f9939d4a0cae4adede24c2af1a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-24 09:00:31 +0530

    Update Help.py
    no

--- HEROKUBOT/plugins/Help.py
+++ HEROKUBOT/plugins/Help.py
@@ -1,5 +1,7 @@
 from pyrogram import filters
-from pyrogram import Client as app
+
+from HEROKUBOT import app
+
 @app.on_message(filters.command(["help"]))
 async def start(client, message):
     await message.reply_text(f"Hello there! I am heroku control bot\n\nHosting Commands: /host\nDelete hosting: /deletehost\n\nCheck hosted apps: /myhost, /heroku.")
