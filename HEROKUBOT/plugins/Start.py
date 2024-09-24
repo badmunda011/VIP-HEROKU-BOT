28fc3486d840b5fcbe7e2ad63b17c6ae8c627aeb
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-24 09:00:48 +0530

    Update Start.py

--- HEROKUBOT/plugins/Start.py
+++ HEROKUBOT/plugins/Start.py
@@ -1,6 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import filters
+from HEROKUBOT import app
 
-@Client.on_message(filters.command(["start"]))
+@app.on_message(filters.command(["start"]))
 async def start(client, message):
     await message.reply_text(f"Hello there! I am heroku control bot\n\nHosting Commands: /host\nDelete hosting: /deletehost\n\nCheck hosted apps: /myhost, /heroku.")
         
