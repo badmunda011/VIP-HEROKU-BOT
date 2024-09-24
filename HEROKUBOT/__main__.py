770ca25d74a8ecad0f9bca589963b74059254e20
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-24 08:58:20 +0530

    Update __main__.py

--- HEROKUBOT/__main__.py
+++ HEROKUBOT/__main__.py
@@ -1,24 +1,8 @@
-import os
-import pyrogram
-import logging
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-import pyromod.listen  # Import pyromod listen
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
 
 # Initialize the bot client
 if __name__ == "__main__" :
     print("Starting Bot...")
-    plugins = dict(root="HEROKUBOT/plugins")
-    app = pyrogram.Client(
-        "HEROKUBOT",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        plugins=plugins
-    )
     app.run()
 
 # To run the bot
