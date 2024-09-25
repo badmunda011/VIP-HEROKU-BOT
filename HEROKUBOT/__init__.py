acc82f207e892451cbd8883d977c551e13a4e43f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 22:27:23 +0530

    Update __init__.py

--- HEROKUBOT/__init__.py
+++ HEROKUBOT/__init__.py
@@ -5,7 +5,7 @@ from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
 import pyromod.listen  # Import pyromod listen
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+                    level=logging.ERROR)
 
 # Initialize the bot client
 plugins = dict(root="HEROKUBOT/plugins")
