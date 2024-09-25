af53a5658015e1ca233ebda44353d5b961ce30a3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 22:33:39 +0530

    Update __main__.py

--- HEROKUBOT/__main__.py
+++ HEROKUBOT/__main__.py
@@ -5,7 +5,7 @@ async def main():
     print("Starting Bot.....")
     await app.start()
     print("Bot Started.....")
-    for file in os.listdir("HEROKUBOT.plugins")
+    for file in os.listdir("HEROKUBOT.plugins"):
         importlib.import_module(f"HEROKUBOT.plugins.{file}")
     await idle()
     await app.stop()
