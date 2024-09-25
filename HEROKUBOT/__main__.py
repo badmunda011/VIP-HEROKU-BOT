e5afc0ed802ed3e03c7bbf735dccf7fa6dc1203f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 22:38:17 +0530

    Update __main__.py

--- HEROKUBOT/__main__.py
+++ HEROKUBOT/__main__.py
@@ -1,16 +1,21 @@
 import importlib
 import asyncio
 import os
-
 from HEROKUBOT import app
+from pyrogram import idle
 
 async def main():
     print("Starting Bot.....")
     await app.start()
     print("Bot Started.....")
-    for file in os.listdir("HEROKUBOT.plugins"):
-        importlib.import_module(f"HEROKUBOT.plugins.{file}")
+
+    for file in os.listdir("HEROKUBOT/plugins"):
+        if file.endswith(".py") and not file.startswith("__"):
+            module_name = file[:-3]
+            importlib.import_module(f"HEROKUBOT.plugins.{module_name}")
+
     await idle()
+    print("Bot Stopped.....")
     await app.stop()
 
 
