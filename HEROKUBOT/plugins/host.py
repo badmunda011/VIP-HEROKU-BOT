db430b8fdba6285bbcc9100f7fada434c7c39e3f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-30 09:28:07 +0530

    Update host.py

--- HEROKUBOT/plugins/host.py
+++ HEROKUBOT/plugins/host.py
@@ -310,14 +310,14 @@ async def ask_for_branch(callback_query, branches, default_branch):
 
 
 # This handles the /host command and displays the repo choice buttons
-@app.on_message(filters.command("host") & filters.private)
+@app.on_message(filters.command("host") & filters.private & SUDOERS)
 async def host_app(client, message):
     global app_name  # Declare global to use it everywhere
     REPO_URL = "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
     await ask_repo_choice(message)
 
 
-@app.on_callback_query(filters.regex(r"deploy_(upstream|external)"))
+@app.on_callback_query(filters.regex(r"deploy_(upstream|external)") & SUDOERS)
 async def handle_repo_choice(client, callback_query):
     global REPO_URL
     choice = callback_query.data.split("_")[1]
@@ -384,7 +384,7 @@ async def handle_repo_choice(client, callback_query):
             return
 
 
-@app.on_callback_query(filters.regex(r"branch_") & filters.user(SUDOERS))
+@app.on_callback_query(filters.regex(r"branch_") & SUDOERS)
 async def handle_branch_selection(client, callback_query):
     global BRANCH_NAME
     BRANCH_NAME = callback_query.data.split("_")[1]
@@ -483,7 +483,7 @@ async def collect_app_info(message):
             ok = await message.reply_text(
                 convert_to_small_caps("⌛ Deploying. Please wait a moment...")
             )
-            
+
             await asyncio.sleep(200)
             await ok.delete()
             await message.reply_text(
@@ -540,7 +540,7 @@ async def get_deployed_apps(client, message):
 # ============================DELETE APP==================================#
 
 
-@app.on_message(filters.command("deletehost") & filters.private)
+@app.on_message(filters.command("deletehost") & filters.private & SUDOERS)
 async def delete_deployed_app(client, message):
     # Fetch the list of deployed apps for the user
     user_apps = await fetch_apps()
